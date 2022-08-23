b88c14b137de9ce5bec5bbd3ca6cff3f310c28e3
Samuelson Owei <samuelsonowei660@gmail.com>
2022-08-23 14:44:33 +0100

    adding server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express")
 const port = process.env.PORT || 3030
 const app = express()
 
+
+app.use(express.static)
+
 app.get("", function(req, res) {
     res.send("Page loaded")
     console.log("connected")
